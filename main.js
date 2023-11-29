894079c71d2f730a784a4939fa4185039be469b9
Juan Esteban Loaiza Lopez <jestebanlpez@gmail.com>
2023-11-28 21:03:03 -0500

    parte uno final

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+let name = document.getElementById("first-name");
+let lastName = document.getElementById("last-name");
+let email = document.getElementById("email");
+let pass = document.getElementById("new-password");
+let image = document.getElementById("profile-picture")
+let age = document.getElementById("age");

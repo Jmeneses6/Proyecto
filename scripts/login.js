aee0bf2ae9a3cbb43371c77f0c4238952e3cf58d
jose Meneses <jmenesesm6@gmail.com>
2023-12-02 12:22:21 -0500

    login provisional

---
+++ scripts/login.js
@@ -0,0 +1,24 @@
+import GetData from "../helpers/getData.js";
+import { USER } from "../helpers/url.js";
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  let email = document.getElementById("email").value;
+
+  let data = await GetData(USER);
+  console.log("entre al login", email, data);
+
+  data?.forEach((element) => {
+    if (email === element.email === element.password) {
+      //----------- Guardar la información en el local para poder pintar el perfil ------//
+      let dataUser = element;
+      sessionStorage.setItem("infoUser", JSON.stringify(dataUser));
+
+      form.reset();
+      window.location.href = "../html/index.html";
+    }
+  });
+});
